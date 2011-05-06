7d28a491d2173c1ed9f66590cc299a1306a7d311
ftao <Filia.Tao@gmail.com>
2011-05-06 09:30:19 +0800

    add icon

--- quickknowhow/js/common.js
+++ quickknowhow/js/common.js
@@ -1,20 +1,23 @@
 var kns = {
-    'web_root' : 'http://k.ftao.org',
+    'sitename' : 'KnowHowSpot',
+    'web_root' : 'http://knowhowspot.com',
     'api_url' : function(api){
         return this.web_root + '/api/v1/' + api;
     },
     'user_url' : function (username){
-        return this.web_root + '/user/' + encodeURIComponent(username) + '.html';
+        return this.web_root + '/users/' + encodeURIComponent(username) + '/';
     }
 };
 
 function show_msg(type, msg, duration){
-  $('#status').removeClass('error').removeClass('success');
+  $('#status').removeClass('error').removeClass('success').removeClass('notice');
   $('#status').addClass(type).html(msg).fadeIn(function(){
-      setTimeout( function(){ 
-            $("#status").fadeOut("fast");
-          }, duration
-      );
+    if(duration > 0){
+        setTimeout( function(){ 
+                $("#status").fadeOut("fast");
+            }, duration
+        );
+    }
   });
 }
 
